550e210dc6aab429621b3418e775e4889f397117
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-04-16 10:37:14 -0300

    Criação do arquivo utils.py com funções para processamento de imagem e extração de pontos de mão utilizando Mediapipe

---
+++ hand_capture/utils.py
@@ -0,0 +1,44 @@
+# utils.py
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_holistic = mp.solutions.holistic     # type: ignore
+mp_drawing = mp.solutions.drawing_utils # type: ignore
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+def detection_mediapipe(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.setflags(write=False)
+    results = model.process(image)
+    image.setflags(write=True)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+def extract_keypoints(results):
+    left_hand = (
+        np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten()
+        if results.left_hand_landmarks else np.zeros(21 * 3)
+    )
+
+    right_hand = (
+        np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten()
+        if results.right_hand_landmarks else np.zeros(21 * 3)
+    )
+
+    return np.concatenate([left_hand, right_hand])
+
+def draw_styled_landmarks(image, results):
+    mp_drawing.draw_landmarks(
+        image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+        mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+        mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+    )
+    mp_drawing.draw_landmarks(
+        image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+        mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+        mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+    )
