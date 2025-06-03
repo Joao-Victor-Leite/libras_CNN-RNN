67055063a379da1543922b23070e5328c8ebc71f
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-06-03 15:41:48 -0300

    atualização dos caminhos dos dados para o modelo LSTM e ajuste das sequências de vídeo

--- config.py
+++ config.py
@@ -1,19 +1,13 @@
 import string
 
-cnn_path_data_train = 'dataset/cnn/train/'
-cnn_path_data_test = 'dataset/cnn/test/'
-
-rnn_path_data_train = 'dataset/rnn/train'
-rnn_path_data_test = 'dataset/rnn/test'
-
-path_data_train = 'pre_processed/train/'
-path_data_test = 'pre_processed/test/'
+path_data_train = 'dataset/lstm/train'
+path_data_test = 'dataset/lstm/test'
 
 
 static_letters = [letter for letter in string.ascii_uppercase if letter not in ['H', 'J', 'X', 'Y', 'Z']]
 dinamic_letters = ['H', 'J', 'X', 'Y', 'Z']
 
 
-train_video_sequence = 30
-test_video_sequence = 6
+train_video_sequence = 80
+test_video_sequence = 20
 frame_sequence = 30
